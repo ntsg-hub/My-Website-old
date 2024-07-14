dc84f72f743340bf41fcbaa6be0c8904aa3e92d8
notsag <gastonmaid2018@gmail.com>
2024-07-14 14:09:45 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+function animateTitle(Title = "Hello, World!", delay = 300) {
+    let counter = 0;
+    let direction = true;
+    aniTitle = setInterval(function () {
+        if (counter === Title.length) {
+            direction = false;
+        }
+        if (counter === 0) {
+            direction = true;
+        }
+    
+        counter = direction ? counter + 1 : counter - 1;
+        let newtitle = (counter == 0) ? " " : Title.slice(0, counter);
+        document.title = newtitle;
+    }, delay)
+}
+
+/* I didn't do it. Thanks to Thomas Weichhart for the help! 🐱 */

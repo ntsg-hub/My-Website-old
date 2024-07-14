755ad0d18ddc5c08d729500c550792e5d39b2d6a
notsag <gastonmaid2018@gmail.com>
2024-07-14 14:15:02 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,17 +2,14 @@ function animateTitle(Title = "Hello, World!", delay = 300) {
     let counter = 0;
     let direction = true;
     aniTitle = setInterval(function () {
-        if (counter === Title.length) {
-            direction = false;
+        if (counter === Title.length || counter === 0) {
+            direction = -direction; // Cambia la dirección al llegar al principio o al final
         }
-        if (counter === 0) {
-            direction = true;
-        }
-    
-        counter = direction ? counter + 1 : counter - 1;
-        let newtitle = (counter == 0) ? " " : Title.slice(0, counter);
-        document.title = newtitle;
-    }, delay)
+        
+        counter += direction;
+        let newTitle = (counter === 0) ? " " : Title.slice(0, counter);
+        document.title = newTitle;
+    }, delay);
 }
 
 /* I didn't do it. Thanks to Thomas Weichhart for the help! 🐱  https://stackoverflow.com/a/68499694 */
